8d3ff77fc06b0e2300522909ab5f9cebc86e033d
pahujajayesh <jayesh.pahuja123@gamil.com>
2022-01-29 12:56:55 +0530

    Added store

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App';
+import { configureStore } from './store';
 
+const store=configureStore();
+console.log('store',store);
+console.log('state',store.getState());
 ReactDOM.render(
   <React.StrictMode>
     <App />
