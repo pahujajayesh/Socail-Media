96a798a330b37112bb355317c0c759af68a24943
pahujajayesh <jayesh.pahuja123@gamil.com>
2022-01-30 12:27:43 +0530

    add react-router 1

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import PostsList from './PostsList';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import { fetchPosts } from '../actions/posts';
+import PostsList from './PostsList';
+import Navbar from './Navbar';
+
+const Login = () => <div>Login</div>;
+const Signup = () => <div>signup</div>;
+const Home = () => <div>home page</div>;
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(fetchPosts());
@@ -13,59 +19,24 @@ class App extends Component {
     const { posts } = this.props;
     return (
       <div>
-        <nav className="nav">
-          <div className="left-div">
-            <img
-              src="https://ninjasfiles.s3.amazonaws.com/0000000000003454.png"
-              alt="logo"
-            />
-          </div>
-          <div className="search-container">
-            <img
-              className="search-icon"
-              src="https://cdn-icons-png.flaticon.com/512/54/54481.png"
-              alt="search-icon"
-            />
-            <input placeholder="Search" />
-
-            <div className="search-results">
-              <ul>
-                <li className="search-results-row">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/149/149995.png"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-                <li className="search-results-row">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/149/149995.png"
-                    alt="user-dp"
-                  />
-                  <span>John Doe</span>
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="right-nav">
-            <div className="user">
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/149/149995.png"
-                alt="user-dp"
-                id="user-dp"
-              />
-              <span>John Doe</span>
-            </div>
-            <div className="nav-links">
-              <ul>
-                <li>Log in</li>
-                <li>Log out</li>
-                <li>Register</li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-        <PostsList posts={posts} />
+        <Router>
+          <Navbar />
+          {/* <PostsList posts={posts} /> */}
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </ul>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Router>
       </div>
     );
   }
--- src/Components/index.js
+++
@@ -1,4 +0,0 @@
-import PostsList from './PostsList';
-import App from './App';
-
-export { PostsList, App };
